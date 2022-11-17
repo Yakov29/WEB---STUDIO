47d4a9d628fceaad933c9ff318bb17841c71d109
Yakov29 <108018683+Yakov29@users.noreply.github.com>
2022-11-17 21:47:45 +0200

    Mobile BETA

---
+++ js/mobile-modal.js
@@ -0,0 +1,20 @@
+ (() => {
+   const refs = {
+        openModalBtn: document.querySelector("[data-open-header-modal]"),
+        closeModalBtn: document.querySelector("[data-close-header-modal]"),
+        backdrop: document.querySelector("[data-header-backdrop]"),
+      };
+
+      refs.openModalBtn.addEventListener("click", toggleModal);
+      refs.closeModalBtn.addEventListener("click", toggleModal);
+
+      refs.backdrop.addEventListener("click", logBackdropClick);
+
+      function toggleModal() {
+        refs.backdrop.classList.toggle("is-hidden");
+      }
+
+      function logBackdropClick() {
+        console.log("Це клік в бекдроп");
+      }
+})()
